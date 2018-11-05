675e821b509d7fd57717f32d32cf3fc75ed22757
Kristen Orue <oruek@oregonstate.edu>
2018-11-05 15:22:26 -0800

    Updated h2 to #ride COunt

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+
+const BASE_URL = "http://zagster-service.herokuapp.com"
 $(updateView)
 
 function updateView() {
+    $.getJSON(BASE_URL + "/rides/count", updateRideCount)
   alert("hopethisworks!")
+}
+
+function updateRideCount(data) {
+    numberOfRides = data.count
+    $("h2#rideCount").html(numberOfRides)
 }
